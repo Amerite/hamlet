7b21ba82244fd1c8dd5c2bc0caa6617c79a171a4
Nomit <nomit@Nomit-ui-MacBook-Pro.local>
2015-07-22 16:37:24 +0900

    Initial Commit

---
+++ Hamlet1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hamlet1
+//
+//  Created by Nomit on 2015. 7. 22..
+//  Copyright (c) 2015년 nomit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
